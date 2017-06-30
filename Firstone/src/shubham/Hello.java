195d56947b139957ad16f887688441fe54a3a5c3
Sachin Kankale <sachin.k@hartleylab.com>
2017-06-30 10:12:21 +0530

    First commit

---
+++ Firstone/src/shubham/Hello.java
@@ -0,0 +1,17 @@
+package shubham;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\hartler\\Downloads\\geckodriver.exe");
+		WebDriver driver ;
+	    driver= new FireFoxdriver (); 
+	      
+	}
+
+}
